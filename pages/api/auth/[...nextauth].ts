9d94775874edc578e14a0f422ab5f891d2d27401
Luis Fraga <luis5944@gmail.com>
2022-05-10 12:33:22 +0200

    1.2.1

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,80 +1,93 @@
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+
 import { dbUsers } from "../../../database";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    // ...add more providers here
+
     Credentials({
       name: "Custom Login",
       credentials: {
         email: {
-          label: "Correo",
+          label: "Correo:",
           type: "email",
-          placeholder: "correo@xxx.com",
+          placeholder: "correo@google.com",
         },
         password: {
-          label: "Contraseña",
+          label: "Contraseña:",
           type: "password",
           placeholder: "Contraseña",
         },
       },
       async authorize(credentials) {
-        console.log(credentials);
+        console.log({ credentials });
+        // return { name: 'Juan', correo: 'juan@google.com', role: 'admin' };
+
         return await dbUsers.checkUserEmailPassword(
           credentials!.email,
           credentials!.password
         );
       },
     }),
+
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
 
-  //Callbacks
-  jwt: {},
-  session: {
-    maxAge: 2592000, //30 días
-    strategy: "jwt",
-    updateAge: 86400, //cada día
+  // Custom Pages
+  pages: {
+    signIn: "/auth/login",
+    newUser: "/auth/register",
   },
+
+  // Callbacks
+  jwt: {
+    // secret: process.env.JWT_SECRET_SEED, // deprecated
+  },
+
+  session: {
+    maxAge: 2592000, /// 30d
+    strategy: "jwt",
+    updateAge: 86400, // cada día
+  },
+
   callbacks: {
     async jwt({ token, account, user }) {
+      // console.log({ token, account, user });
+
       if (account) {
         token.accessToken = account.access_token;
+
         switch (account.type) {
           case "oauth":
-            // comprobar si existe en la bd o crearlo
             token.user = await dbUsers.oAuthToDbUser(
               user?.email || "",
               user?.name || ""
             );
-
             break;
+
           case "credentials":
             token.user = user;
             break;
-
-          default:
-            break;
         }
       }
+
       return token;
     },
-    async session({ session, user, token }) {
+
+    async session({ session, token, user }) {
+      // console.log({ session, token, user });
+
       session.accessToken = token.accessToken;
       session.user = token.user as any;
 
       return session;
     },
-  },
-  // custom pages
-  pages: {
-    signIn: "/auth/login",
-    newUser: "/auth/register",
   },
 });
